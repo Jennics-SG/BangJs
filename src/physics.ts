09acb9dfbe498a0eddf06386187a64306b4e16d8
Jimy <jimyhoulbrook@gmail.com>
2024-04-30 12:52:27 +0100

    Changed B2D loading
    Done with public async function now

--- src/physics.ts
+++ src/physics.ts
@@ -6,14 +6,22 @@
 
 import B2D from './B2D/Box2D_v2.3.1_min';
 
-// 1: Download b2d using b2d factory
+/** Get Box2D instance */
+export async function getB2D(){
+    return new Promise((res, rej) => {
+        B2D().then(box => {
+            res(box);
+        })
+    });
+}
 
 export class PhysicsLayer{
     constructor(){
-        (async ()=> {
-            B2D().then(box2D => {
-                console.log(box2D);
-            });
-        })();
+        // Download Box2D from
+        this.init();
+    }
+    
+    async init(){
+        console.log(await getB2D());
     }
 }
