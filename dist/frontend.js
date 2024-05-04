c8fbcd671ef8d17a31ac320781817c3c830abec7
Jimy <jimyhoulbrook@gmail.com>
2024-05-04 19:15:54 +0100

    it works init
    lolll

--- dist/frontend.js
+++ dist/frontend.js
@@ -43,56 +43,8 @@ class PhysicsTest{
             density: 0,
         }
 
-        const groundSprite = new Bang.Sprites.StaticSprite(
-            250, 250, 
-            Bang.Assets.get("missing"), 
-            100, 20
-        );
-        const groundEntity = new Bang.Physics.Entity(
-            250, 250, 
-            groundSprite.width, groundSprite.height, 
-            groundOps, this.app.engine, this.app._physicsLayers[0], "ground"
-        );
-        groundEntity.sprite = groundSprite
-
-        this.app.addChild(groundSprite)
-
-        document.addEventListener('click', this.placeBox.bind(this));
-    }
-
-    async placeBox(e){
-        const x = e.clientX;
-        const y = e.clientY;
-
-        // Is in bounds
-        const appBounds = this.app.getWidthHeight();
-        if(!x <= appBounds.x && !y <= appBounds.y) return
-
-        console.log(x, y)
-        console.log(this.app.engine.coOrdPixelToWorld(x, y));
-
-        // Create Box
-        const box = new Bang.Sprites.StaticSprite(x, y, Bang.Assets.get('missing'), 50, 50);
-
-        // Create Physics Entity
-        const entityOps = {
-            bodyType: "Dynamic",
-            shape: "box",
-            density: 10,
-            friction: 0,
-            restitutiuon: 0
-        }
-
-        const entity = new Bang.Physics.Entity(
-            x, y, 
-            box.width, box.height, 
-            entityOps, this.app.engine, this.app._physicsLayers[0], "box"
-        );
-
-        // Add Box to physics Entity
-        entity.sprite = box;
-
-        this.app.addChild(box);
+        // Movable Sprite
+        // movement function needs to apply force rather than move position
     }
 }
 
